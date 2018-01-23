0320d444d1cfec48211f9a9037bde6501caaa3e2
Sam Sturtevant <samstur@uw.edu>
2018-01-22 21:52:30 -0800

    Finished basic ops

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -10,4 +10,23 @@ import Foundation
 
 // All your work will go in here
 class Calculator {
+    public func add(lhs : Int, rhs : Int) -> Int {
+        return lhs + rhs;
+    }
+    
+    public func subtract(lhs : Int, rhs : Int) -> Int {
+        return lhs - rhs;
+    }
+    
+    public func multiply(lhs : Int, rhs : Int) -> Int {
+        return lhs * rhs;
+    }
+    
+    public func divide(lhs : Int, rhs : Int) -> Int {
+        return lhs / rhs;
+    }
+    
+//    public func mathOp(lhs: Int, rhs : Int, op : Any) -> Int {
+//        
+//    }
 }
